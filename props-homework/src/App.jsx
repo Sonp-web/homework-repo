f9a4389dcd8c255e9aee5bbd3687e411509724ab
Егор Ничипор <egornicipor@MacBook-Air>
2026-02-14 23:18:51 +0300

    update homework

--- props-homework/src/App.jsx
+++ props-homework/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
-import Card from "./Card";
-
+import Age from "./Age";
+import Name from "./Name";
+import Male from "./Male";
+import Sports from "./Sports";
+import SayHi from "./SayHi";
+import Childs from "./Childs";
 function App() {
   return (
     <>
-      <Card
-        age={12}
-        name={"Egor"}
-        male={true}
-        sport={{ voleyball: true, football: false }}
-        hello={() => "Hi"}
-        childs={["Masha", "Dasha"]}
-      />
+      <Age age={12} />
+      <Name name={"Egor"} />
+      <Male male={true} />
+      <Sports sports={{ voleyball: true }} />
+      <SayHi sayHi={() => "Hi"} />
+      <Childs childs={["Dasha", "Masha"]} />
     </>
   );
 }
---
+++ props-homework/src/Male.jsx
@@ -0,0 +1,4 @@
+const Male = ({ male }) => {
+  return <h2>{male ? "true" : "false"}</h2>;
+};
+export default Male;
