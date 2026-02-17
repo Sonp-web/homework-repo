6ba9c7d5a9369d2e14d8eaf8dc8a3fb4665cd155
Егор Ничипор <egornicipor@MacBook-Air>
2026-02-17 03:19:21 +0300

    end state-3

---
+++ state-3/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Basket from "./Basket";
+import Profile from "./Profile";
+import TaskList from "./TaskList";
+
+function App() {
+  return (
+    <>
+      <Profile></Profile>
+      <TaskList></TaskList>
+      <Basket></Basket>
+    </>
+  );
+}
+
+export default App;
---
+++ state-3/src/TaskList.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./AllStyle.css";
+function TaskList() {
+  const [tasks, setTasks] = useState(["Купить хлеб", "Погулять с собакой"]);
+  return (
+    <div className="wrapper">
+      <h2>Список задач</h2>
+      <ul>
+        {tasks.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
+      <button
+        onClick={() =>
+          setTasks((oldTasks) => {
+            let length = Math.floor(Math.random() * 10) + 1;
+            let str = "";
+            while (str.length < length) {
+              str += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+            }
+            return [...oldTasks, str];
+          })
+        }
+      >
+        Добавить задачу
+      </button>
+      <button onClick={() => setTasks((oldTasks) => oldTasks.slice(0, -1))}>
+        Удалить последнюю задачу
+      </button>
+    </div>
+  );
+}
+export default TaskList;
