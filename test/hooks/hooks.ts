9b343d9ff52c750ca2c56568fcf5b7951774cd75
Егор Ничипор <egornicipor@MacBook-Air>
2026-06-10 16:33:47 +0300

    registration TS

---
+++ test/hooks/hooks.ts
@@ -0,0 +1,8 @@
+import { AppDispatch, RootState } from "../redux/store";
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- todo/src/Header.tsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-  return (
-    <>
-      <h1>Мой To-Do list</h1>
-    </>
-  );
-};
-export default Header;
